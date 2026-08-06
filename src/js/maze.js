2ddbedd0bdcb5b6481f7358eae9b62bad10dfd87
Maxir <maxir.dev@gmail.com>
2026-08-05 23:04:27 -0300

    01-Update ghost personalities with release timings in maze.js

--- src/js/maze.js
+++ src/js/maze.js
@@ -52,8 +52,10 @@ const MAZE = MAZE_STR.map( ( row ) => row.split( '' ).map( parseTile ) );
 const TUNNEL_ROW = 14;
 const PACMAN_START = { x: 13, y: 23 };
 const GHOST_STARTS = [
-  { x: 13, y: 14, kind: 'hunter' }, // dentro de la pen
-  { x: 14, y: 14, kind: 'random' }, // dentro de la pen
+  { x: 13, y: 14, kind: 'hunter' },   // liberado en t=0
+  { x: 14, y: 14, kind: 'ambusher' }, // t aprox 5 s
+  { x: 13, y: 15, kind: 'flanker' },  // t aprox 10 s
+  { x: 14, y: 15, kind: 'wanderer' }, // t aprox 15 s
 ];
 
 window.MAZE = MAZE;
